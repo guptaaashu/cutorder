1543d1f5527b9791bdd57e83587cfbf76f539fe6
guptaaashu <guptaaashuid@gmail.com>
2020-02-28 19:48:03 +0530

    added

--- order/views.py
+++ order/views.py
@@ -31,7 +31,7 @@ def laye(request):
         print(form)
         if form.is_valid():
             new=form.save()
-            return redirect('red2',pk=new.pk)
+            return redirect('red3',pk=new.pk)
     else:
         form = LayForm()
     return render(request, 'lay.html', {'form': form})
@@ -727,6 +727,341 @@ def lay_detail_view_yellow2(request,pk):
     u=zip(l,t,y,g,te)
     method=2
     color='red'
+    return render(request, 'lay_detail.html', {'data': u,'lay': oay,'method':method,'color':color})
+
+
+def lay_detail_view_red3(request,pk):
+    oay=get_object_or_404(lay, pk=pk)
+    def check(bal):
+        for i in bal:
+            if(i[1]>3):
+                return True
+        return False
+    def getlay(l,red,t,y,g):
+        bal=[]
+        for i in red:
+            f=[]
+            f.append(i[0])
+            f.append(i[1])
+            bal.append(f)
+        while(check(red)):
+            red.sort(reverse=True,key=lambda x:x[1])
+            ti=False
+            s=0
+            for j in range(0,8):
+                if(red[j][1]==red[j+1][1] and red[j][1]>3):
+                    s=red[j][1]
+                    ti=True
+                    break
+            if(ti==True):
+                l.append(s)
+                c=[]
+                u=[]
+                for i in range(0,9):
+                    if(red[i][1]>=s):
+                        red[i][1]=red[i][1]-s
+                        u.append(red[i][0])
+                red.sort(key=lambda x:x[0])
+                for i in bal:
+                    c.append(i[1])
+                y.append(c)
+                bal=[]
+                d=[]
+                for i in red:
+                    d.append(i[1])
+                    f=[]
+                    f.append(i[0])
+                    f.append(i[1])
+                    bal.append(f)
+                t.append(d)
+                u.sort()
+                g.append(u)
+            else:
+                red.sort(reverse=True,key=lambda x:x[1])
+                st=red[0][1]
+                yt=int(st/10)
+                ytt=yt+1
+                yt=yt*10
+                ytt=ytt*10
+                se=set()
+                for i in range(0,9):
+                    if(red[i][1]<ytt and red[i][1]>=yt):
+                        se.add(red[i][1])
+                se=list(se)
+                se.sort()
+                leng=len(se)
+                ue=int(leng/2)
+                s=se[ue]
+                if(s<=3):
+                    break
+                l.append(s)
+                u=[]
+                for i in range(0,9):
+                    if(red[i][1]<ytt and red[i][1]>=yt and red[i][1]>0):
+                        if(s>10):
+                            red[i][1]=red[i][1]-s
+                            u.append(red[i][0])
+                        else:
+                            while(red[i][1]>2):
+                                red[i][1]=red[i][1]-s
+                                u.append(red[i][0])      
+                c=[]
+                red.sort(key=lambda x:x[0])
+                for i in bal:
+                    c.append(i[1])
+                y.append(c)
+                bal=[]
+                d=[]
+                for i in red:
+                    d.append(i[1])
+                    f=[]
+                    f.append(i[0])
+                    f.append(i[1])
+                    bal.append(f)
+                t.append(d)
+                u.sort()
+                g.append(u)
+        print(l)
+        print(t)
+        print(y)
+        print(g)
+    red=[[38,oay.Red38],[40,oay.Red40],[42,oay.Red42],[44,oay.Red44],[46,oay.Red46],[48,oay.Red48],[50,oay.Red50],[52,oay.Red52],[54,oay.Red54]]
+    l=[]
+    t=[]
+    y=[]
+    g=[]
+    getlay(l,red,t,y,g)
+    te=[]
+    it=1
+    for i in l:
+        te.append(it)
+        it=it+1
+    u=zip(l,t,y,g,te)
+    method=2
+    color='red'
+    return render(request, 'lay_detail.html', {'data': u,'lay': oay,'method':method,'color':color})
+
+
+def lay_detail_view_blue3(request,pk):
+    oay=get_object_or_404(lay, pk=pk)
+    def check(bal):
+        for i in bal:
+            if(i[1]>3):
+                return True
+        return False
+    def getlay(l,red,t,y,g):
+        bal=[]
+        for i in red:
+            f=[]
+            f.append(i[0])
+            f.append(i[1])
+            bal.append(f)
+        while(check(red)):
+            red.sort(reverse=True,key=lambda x:x[1])
+            ti=False
+            s=0
+            for j in range(0,8):
+                if(red[j][1]==red[j+1][1] and red[j][1]>3):
+                    s=red[j][1]
+                    ti=True
+                    break
+            if(ti==True):
+                l.append(s)
+                c=[]
+                u=[]
+                for i in range(0,9):
+                    if(red[i][1]>=s):
+                        red[i][1]=red[i][1]-s
+                        u.append(red[i][0])
+                red.sort(key=lambda x:x[0])
+                for i in bal:
+                    c.append(i[1])
+                y.append(c)
+                bal=[]
+                d=[]
+                for i in red:
+                    d.append(i[1])
+                    f=[]
+                    f.append(i[0])
+                    f.append(i[1])
+                    bal.append(f)
+                t.append(d)
+                u.sort()
+                g.append(u)
+            else:
+                red.sort(reverse=True,key=lambda x:x[1])
+                st=red[0][1]
+                yt=int(st/10)
+                ytt=yt+1
+                yt=yt*10
+                ytt=ytt*10
+                se=set()
+                for i in range(0,9):
+                    if(red[i][1]<ytt and red[i][1]>=yt):
+                        se.add(red[i][1])
+                se=list(se)
+                se.sort()
+                leng=len(se)
+                ue=int(leng/2)
+                s=se[ue]
+                if(s<=3):
+                    break
+                l.append(s)
+                u=[]
+                for i in range(0,9):
+                    if(red[i][1]<ytt and red[i][1]>=yt and red[i][1]>0):
+                        if(s>10):
+                            red[i][1]=red[i][1]-s
+                            u.append(red[i][0])
+                        else:
+                            while(red[i][1]>2):
+                                red[i][1]=red[i][1]-s
+                                u.append(red[i][0])      
+                c=[]
+                red.sort(key=lambda x:x[0])
+                for i in bal:
+                    c.append(i[1])
+                y.append(c)
+                bal=[]
+                d=[]
+                for i in red:
+                    d.append(i[1])
+                    f=[]
+                    f.append(i[0])
+                    f.append(i[1])
+                    bal.append(f)
+                t.append(d)
+                u.sort()
+                g.append(u)
+        print(l)
+        print(t)
+        print(y)
+        print(g)
+    red=[[38,oay.Blue38],[40,oay.Blue40],[42,oay.Blue42],[44,oay.Blue44],[46,oay.Blue46],[48,oay.Blue48],[50,oay.Blue50],[52,oay.Blue52],[54,oay.Blue54]]
+    l=[]
+    t=[]
+    y=[]
+    g=[]
+    getlay(l,red,t,y,g)
+    te=[]
+    it=1
+    for i in l:
+        te.append(it)
+        it=it+1
+    u=zip(l,t,y,g,te)
+    method=2
+    color='Blue'
+    return render(request, 'lay_detail.html', {'data': u,'lay': oay,'method':method,'color':color})
+
+def lay_detail_view_yellow3(request,pk):
+    oay=get_object_or_404(lay, pk=pk)
+    def check(bal):
+        for i in bal:
+            if(i[1]>3):
+                return True
+        return False
+    def getlay(l,red,t,y,g):
+        bal=[]
+        for i in red:
+            f=[]
+            f.append(i[0])
+            f.append(i[1])
+            bal.append(f)
+        while(check(red)):
+            red.sort(reverse=True,key=lambda x:x[1])
+            ti=False
+            s=0
+            for j in range(0,8):
+                if(red[j][1]==red[j+1][1] and red[j][1]>3):
+                    s=red[j][1]
+                    ti=True
+                    break
+            if(ti==True):
+                l.append(s)
+                c=[]
+                u=[]
+                for i in range(0,9):
+                    if(red[i][1]>=s):
+                        red[i][1]=red[i][1]-s
+                        u.append(red[i][0])
+                red.sort(key=lambda x:x[0])
+                for i in bal:
+                    c.append(i[1])
+                y.append(c)
+                bal=[]
+                d=[]
+                for i in red:
+                    d.append(i[1])
+                    f=[]
+                    f.append(i[0])
+                    f.append(i[1])
+                    bal.append(f)
+                t.append(d)
+                u.sort()
+                g.append(u)
+            else:
+                red.sort(reverse=True,key=lambda x:x[1])
+                st=red[0][1]
+                yt=int(st/10)
+                ytt=yt+1
+                yt=yt*10
+                ytt=ytt*10
+                se=set()
+                for i in range(0,9):
+                    if(red[i][1]<ytt and red[i][1]>=yt):
+                        se.add(red[i][1])
+                se=list(se)
+                se.sort()
+                leng=len(se)
+                ue=int(leng/2)
+                s=se[ue]
+                if(s<=3):
+                    break
+                l.append(s)
+                u=[]
+                for i in range(0,9):
+                    if(red[i][1]<ytt and red[i][1]>=yt and red[i][1]>0):
+                        if(s>10):
+                            red[i][1]=red[i][1]-s
+                            u.append(red[i][0])
+                        else:
+                            while(red[i][1]>2):
+                                red[i][1]=red[i][1]-s
+                                u.append(red[i][0])      
+                c=[]
+                red.sort(key=lambda x:x[0])
+                for i in bal:
+                    c.append(i[1])
+                y.append(c)
+                bal=[]
+                d=[]
+                for i in red:
+                    d.append(i[1])
+                    f=[]
+                    f.append(i[0])
+                    f.append(i[1])
+                    bal.append(f)
+                t.append(d)
+                u.sort()
+                g.append(u)
+        print(l)
+        print(t)
+        print(y)
+        print(g)
+    red=[[38,oay.Yellow38],[40,oay.Yellow40],[42,oay.Yellow42],[44,oay.Yellow44],[46,oay.Yellow46],[48,oay.Yellow48],[50,oay.Yellow50],[52,oay.Yellow52],[54,oay.Yellow54]]
+    l=[]
+    t=[]
+    y=[]
+    g=[]
+    getlay(l,red,t,y,g)
+    te=[]
+    it=1
+    for i in l:
+        te.append(it)
+        it=it+1
+    u=zip(l,t,y,g,te)
+    method=2
+    color='Yellow'
     return render(request, 'lay_detail.html', {'data': u,'lay': oay,'method':method,'color':color})
 
 
